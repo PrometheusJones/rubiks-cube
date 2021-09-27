07063cd28a577232c28510a8e2bba3af6ca3d9b1
IvanAtanasov <88195253+PrometheusJones@users.noreply.github.com>
2021-09-27 14:25:00 +0300

    extract handlebars config

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+const initHandlebars = require('./config/handlebars.js');
+//require('./config/handlebars.js')(app);
 
 const app = express();
 
-app.set('views', path.resolve('./src/views'))
+initHandlebars(app);
 
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}));
-
-app.set('view engine', 'hbs')
 
 app.all('/', (req, res) => {
     res.render('index', { layout: false })
