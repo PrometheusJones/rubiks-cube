eb6b60348d047f30d4b4174e8286be49b305c8a7
IvanAtanasov <88195253+PrometheusJones@users.noreply.github.com>
2021-09-28 14:16:22 +0300

    browse cubes

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -3,11 +3,18 @@ const uniqid = require('uniqid')
 class Cube {
     static #cubes = [ //Private Static 
         {
-            id: 'asdasdasda',
+            id: 'f2j8et83nsku3zdvas',
             name: 'Mirror',
             descriptino: 'mirror cube',
             imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
             difficulty: '4'
+        },
+        {
+            id: '3mj8et83nsku3zdve0',
+            name: 'Ice Cube',
+            descriptino: 'Very cold cube',
+            imageUrl: 'https://img.discogs.com/oQsbgXka4xaIMzsBvFO3VUyvtIM=/fit-in/600x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-349388-1489891729-6218.jpeg.jpg',
+            difficulty: '6'
         }
     ];
 
