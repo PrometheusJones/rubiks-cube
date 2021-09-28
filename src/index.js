7480de28746ebdb90a62901e84a227dab5b3bb7b
IvanAtanasov <88195253+PrometheusJones@users.noreply.github.com>
2021-09-28 12:03:03 +0300

    add controlers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const initHandlebars = require('./config/handlebars.js');
 const path = require('path');
-
+const routes = require('./routes.js')
 //require('./config/handlebars.js')(app);
 
 const app = express();
@@ -9,9 +9,6 @@ const app = express();
 initHandlebars(app);
 
 app.use(express.static(path.resolve(__dirname, './public')))
-
-app.all('/', (req, res) => {
-    res.render('index')
-});
+app.use(routes);
 
 app.listen(5000, console.log.bind(console, 'Application is running on http://localhost:5000'));
