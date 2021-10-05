30b79da7b64b04036c4e82a40f353c1ef55dc57c
IvanAtanasov <88195253+PrometheusJones@users.noreply.github.com>
2021-10-05 15:54:46 +0300

    add mongoose ,add cube model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const initHandlebars = require('./config/handlebars.js');
 const path = require('path');
 const routes = require('./routes.js');
-const config = require('./config/config.json')[process.env.NODE_ENV]
+const config = require('./config/config.json')[process.env.NODE_ENV];
+const initDatabase = require('./config/database.js');
+
 //require('./config/handlebars.js')(app);
 
 const app = express();
@@ -14,4 +16,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve(__dirname, './public')))
 app.use(routes);
 
-app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
+initDatabase(config.DB_CONNECTION_STRING)
+    .then(() => {
+        app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
+    })
+    .catch(err => {
+        console.log('Application init failed:', err);
+    })
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,46 +1,34 @@
-const uniqid = require('uniqid')
+const mongoose = require('mongoose');
 
-class Cube {
-    //Private Static
-    static #cubes = [
-        {
-            id: 'f2j8et83nsku3zdvas',
-            name: 'Mirror',
-            description: 'mirror cube',
-            imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
-            difficulty: '4'
-        },
-        {
-            id: '3mj8et83nsku3zdve0',
-            name: 'Ice Cube',
-            description: 'Very cold cube',
-            imageUrl: 'https://img.discogs.com/oQsbgXka4xaIMzsBvFO3VUyvtIM=/fit-in/600x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-349388-1489891729-6218.jpeg.jpg',
-            difficulty: '6'
-        },
-        {
-            id: '3mj8et87wcku44w8t2',
-            name: 'Panda Cube',
-            description: 'Eats cubeboo(cubic bamboo)',
-            imageUrl: 'https://i.pinimg.com/564x/94/6d/8a/946d8aeb4fab6be818067fb9dd2461a5--pandas-cubes.jpg',
-            difficulty: '4'
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        //validate: /^https?:\/\//i
+        validate: {
+            validator: function (value) {
+                return /^https?:\/\//i.test(value)
+            },
+            message: 'Image url is invalid!'
         }
-    ];
-
-    constructor(name, description, imageUrl, difficulty) {
-        this.id = uniqid();
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficulty = difficulty;
+    },
+    difficulty: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6,
     }
+});
 
-    static getAll() {
-        return Cube.#cubes.slice();
-    }
-
-    static add(cube) {
-        Cube.#cubes.push(cube)
-    }
-}
+const Cube = mongoose.model('Cube', cubeSchema)
 
 module.exports = Cube
