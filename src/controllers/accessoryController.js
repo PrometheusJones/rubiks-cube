f402eea0e4840bdc0905f085bc21dfed4855c2f0
IvanAtanasov <88195253+PrometheusJones@users.noreply.github.com>
2021-10-06 13:26:14 +0300

    accessory page

---
+++ src/controllers/accessoryController.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/create', (req, res) => {
+    res.render('accessory/create');
+
+});
+
+router.post('/create', (req, res) => {
+    let accessory = req.body;
+
+    console.log(accessory);
+    res.end();
+});
+
+
+
+module.exports = router;
